672dd4fc8395959af9a69a63194358422ff9365e
comjun04 <comjun04@gmail.com>
2020-05-06 08:37:54 +0900

    koreanbots

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const dbkr = require('dbkr')
+const koreanbots = require('koreanbots')
 const Extension = require('../../classes/Extension')
 
-class DBKRExtension extends Extension {
+class KoreanbotsExtension extends Extension {
   constructor(client) {
     super(client, {
-      name: 'dbkr',
-      description: 'Extension for DBKR'
+      name: 'koreanbots',
+      description: 'Extension for Koreanbots'
     })
+
+    if(typeof this._config.token !== 'string' || this._config.token.length < 1) return this._logger.error('Invalid token provided. Extension stopped.')
+
+    this.bot = new koreanbots.MyBot(this._config.token)
 
     this.recentGuildCount = 0
     this.saveGuildCount()
+    setInterval(this.saveGuildCount, this._config.saveInterval || 10000)
   }
 
   saveGuildCount() {
-    setInterval(() => {
-      const guildCount = this._client.guilds.cache.size
-      if(guildCount === this.recentGuildCount) return this._logger.debug('Guild Count same as before: ' + guildCount + ', Skipping.')
+    const guildCount = this._client.guilds.cache.size
+    if(guildCount === this.recentGuildCount) return this._logger.debug('Guild Count same as before: ' + guildCount + ', Skipping.')
 
-      if(typeof this._config.token !== 'string' || this._config.token.length < 1) return this._logger.error('Invalid token provided.')
-      dbkr(this._client, this._config.token || '')
-        .then(() => {
-          this._logger.log('Guild Count updated: ' + guildCount)
-          this.recentGuildCount = guildCount
-        })
-        .catch((err) => this._logger.error(err))
-    }, this._config.saveInterval || 10000)
+    if(typeof this._config.token !== 'string' || this._config.token.length < 1) return this._logger.error('Invalid token provided.')
+    this.bot.update(guildCount)
+      .then(() => {
+        this._logger.log('Guild Count updated: ' + guildCount)
+        this.recentGuildCount = guildCount
+      })
+      .catch((err) => this._logger.error(err))
   }
 }
 
-module.exports = DBKRExtension
+module.exports = KoreanbotsExtension
